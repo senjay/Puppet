7443a078dc8ff30924138b744bfda6e02ebb8745
senjay <760832791@qq.com>
2022-05-24 17:03:02 +0800

    add Shader

---
+++ Puppet/src/Puppet/Renderer/Shader.cpp
@@ -0,0 +1,121 @@
+#include "PPpch.h"
+#include "Shader.h"
+#include <glad/glad.h>
+namespace Puppet {
+	Shader::Shader(const std::string& vertexSrc, const std::string& fragmentSrc)
+	{
+		// Create an empty vertex shader handle
+		GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+		// Send the vertex shader source code to GL
+		// Note that std::string's .c_str is NULL character terminated.
+		const GLchar* source = vertexSrc.c_str();
+		glShaderSource(vertexShader, 1, &source, 0);
+
+		// Compile the vertex shader
+		glCompileShader(vertexShader);
+
+		// 顶点着色器编译状态
+		GLint isCompiled = 0;
+		glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &isCompiled);
+		if (isCompiled == GL_FALSE)
+		{
+			GLint maxLength = 0;
+			glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &maxLength);
+
+			// The maxLength includes the NULL character
+			std::vector<GLchar> infoLog(maxLength);
+			glGetShaderInfoLog(vertexShader, maxLength, &maxLength, &infoLog[0]);
+
+			// We don't need the shader anymore
+			glDeleteShader(vertexShader);
+
+			PP_CORE_ERROR("{0}", infoLog.data());
+			PP_CORE_ASSERT(false, "Vertex shader compilation failure!");
+			return;
+		}
+
+		// Create an empty fragment shader handle
+		GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+		// Send the fragment shader source code to GL
+		// Note that std::string's .c_str is NULL character terminated.
+		source = fragmentSrc.c_str();
+		glShaderSource(fragmentShader, 1, &source, 0);
+
+		// Compile the fragment shader
+		glCompileShader(fragmentShader);
+
+		// 片段着色器状态
+		glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &isCompiled);
+		if (isCompiled == GL_FALSE)
+		{
+			GLint maxLength = 0;
+			glGetShaderiv(fragmentShader, GL_INFO_LOG_LENGTH, &maxLength);
+
+			// The maxLength includes the NULL character
+			std::vector<GLchar> infoLog(maxLength);
+			glGetShaderInfoLog(fragmentShader, maxLength, &maxLength, &infoLog[0]);
+
+			// We don't need the shader anymore
+			glDeleteShader(fragmentShader);
+			// Either of them, Don't leak shaders
+			glDeleteShader(vertexShader);
+
+			PP_CORE_ERROR("{0}", infoLog.data());
+			PP_CORE_ASSERT(false, "Fragment shader compilation failure!");
+			return;
+		}
+
+		// Vertex and fragment shaders are successfully compiled.
+		// Now time to link them together into a program.
+		// Get a program object.
+		m_RendererID = glCreateProgram();
+
+		// 将编译好的shader放到程序里面
+		glAttachShader(m_RendererID, vertexShader);
+		glAttachShader(m_RendererID, fragmentShader);
+
+		// Link our program
+		glLinkProgram(m_RendererID);
+
+		// Note the different functions here: glGetProgram* instead of glGetShader*
+		GLint isLinked = 0;
+		glGetProgramiv(m_RendererID, GL_LINK_STATUS, (int*)&isLinked);
+		if (isLinked == GL_FALSE)
+		{
+			GLint maxLength = 0;
+			glGetProgramiv(m_RendererID, GL_INFO_LOG_LENGTH, &maxLength);
+
+			// The maxLength includes the NULL character
+			std::vector<GLchar> infoLog(maxLength);
+			glGetShaderInfoLog(m_RendererID, maxLength, &maxLength, &infoLog[0]);
+
+			// We don't need the program anymore.
+			glDeleteProgram(m_RendererID);
+			// Don't leak shaders either.
+			glDeleteShader(vertexShader);
+			glDeleteShader(fragmentShader);
+
+			PP_CORE_ERROR("{0}", infoLog.data());
+			PP_CORE_ASSERT(false, "Shader link failure!");
+			return;
+		}
+
+		// Always detach shaders after a successful link.
+		glDetachShader(m_RendererID, vertexShader);
+		glDetachShader(m_RendererID, fragmentShader);
+	}
+	Shader::~Shader()
+	{
+		glDeleteProgram(m_RendererID);
+	}
+	void Shader::Bind() const
+	{
+		glUseProgram(m_RendererID);
+	}
+	void Shader::UnBind() const
+	{
+		glUseProgram(0);
+	}
+}
