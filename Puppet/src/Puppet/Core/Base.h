7ee3d56e3f83ec543391b305414770bdc7ef9f78
senjay <760832791@qq.com>
2022-06-14 22:56:41 +0800

    add intrusive Ref

--- Puppet/src/Puppet/Core/Base.h
+++ Puppet/src/Puppet/Core/Base.h
@@ -1,23 +1,24 @@
 #pragma once
 #include <memory>
+#include "Puppet/Utils/Ref.h"
 namespace Puppet {
 
 	template<typename T>
 	using Scope = std::unique_ptr<T>;
-	template<typename T, typename ... Args>
 
+	template<typename T, typename ... Args>
 	constexpr Scope<T> CreateScope(Args&& ... args)
 	{
 		return std::make_unique<T>(std::forward<Args>(args)...);
 	}
-	template<typename T>
-	using Ref = std::shared_ptr<T>;
+
 
 	template<typename T, typename ... Args>
 	constexpr Ref<T> CreateRef(Args&& ... args)
 	{
-		return std::make_shared<T>(std::forward<Args>(args)...);
+		return Ref<T>::Create(std::forward<Args>(args)...);
 	}
+
 }
 
 #ifdef  PP_PLATFORM_WINDOWS
