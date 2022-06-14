719f0211234347fd421a93bd78bab67a0ac7b080
senjay <760832791@qq.com>
2022-06-14 22:56:41 +0800

    add intrusive Ref

---
+++ Puppet/src/Puppet/Utils/Ref.h
@@ -0,0 +1,164 @@
+#pragma once
+
+#include <stdint.h>
+#include <atomic>
+namespace Puppet {
+
+	class RefCounted
+	{
+	public:
+		void IncRefCount() const
+		{
+			m_RefCount++;
+		}
+		void DecRefCount() const
+		{
+			m_RefCount--;
+		}
+
+		uint32_t GetRefCount() const { return m_RefCount; }
+	private:
+		mutable std::atomic<uint32_t> m_RefCount = 0;
+	};
+
+	/*PS:
+		1.Reset(T* instance) should IncRef()???
+		2.未重写operator=(T* instance),所以调用时它时会通过隐式构造函数和赋值拷贝函数间接实现
+		3. Base class cast
+	*/
+	template<typename T>
+	class Ref
+	{
+	public:
+		Ref()
+			: m_Instance(nullptr)
+		{
+		}
+
+		Ref(std::nullptr_t n)
+			: m_Instance(nullptr)
+		{
+		}
+
+		Ref(T* instance)
+			: m_Instance(instance)
+		{
+			static_assert(std::is_base_of<RefCounted, T>::value, "Class is not RefCounted!");
+
+			IncRef();
+		}
+
+		template<typename T2>
+		Ref(const Ref<T2>& other)
+		{
+			m_Instance = (T*)other.m_Instance;
+			IncRef();
+		}
+
+		template<typename T2>
+		Ref(Ref<T2>&& other)
+		{
+			m_Instance = (T*)other.m_Instance;
+			other.m_Instance = nullptr;
+		}
+
+		~Ref()
+		{
+			DecRef();
+		}
+
+		Ref(const Ref<T>& other)
+			: m_Instance(other.m_Instance)
+		{
+			IncRef();
+		}
+
+		Ref& operator=(std::nullptr_t)
+		{
+			DecRef();
+			m_Instance = nullptr;
+			return *this;
+		}
+
+		Ref& operator=(const Ref<T>& other)
+		{
+			other.IncRef();
+			DecRef();
+
+			m_Instance = other.m_Instance;
+			return *this;
+		}
+
+		template<typename T2>
+		Ref& operator=(const Ref<T2>& other)
+		{
+			other.IncRef();
+			DecRef();
+
+			m_Instance = other.m_Instance;
+			return *this;
+		}
+
+		template<typename T2>
+		Ref& operator=(Ref<T2>&& other)
+		{
+			DecRef();
+
+			m_Instance = other.m_Instance;
+			other.m_Instance = nullptr;
+			return *this;
+		}
+
+		operator bool() { return m_Instance != nullptr; }
+		operator bool() const { return m_Instance != nullptr; }
+
+		T* operator->() { return m_Instance; }
+		const T* operator->() const { return m_Instance; }
+
+		T& operator*() { return *m_Instance; }
+		const T& operator*() const { return *m_Instance; }
+
+		T* get() { return  m_Instance; }
+		const T* get() const { return  m_Instance; }
+		
+		void Reset(T* instance = nullptr)
+		{
+			DecRef();
+			m_Instance = instance;
+		}
+
+		template<typename... Args>
+		static Ref<T> Create(Args&&... args)
+		{
+			return Ref<T>(new T(std::forward<Args>(args)...));
+		}
+	private:
+		void IncRef() const
+		{
+			if (m_Instance)
+				m_Instance->IncRefCount();
+		}
+
+		void DecRef() const
+		{
+			if (m_Instance)
+			{
+				m_Instance->DecRefCount();
+				if (m_Instance->GetRefCount() == 0)
+				{
+					delete m_Instance;
+				}
+			}
+		}
+
+		template<class T2>
+		friend class Ref;
+		T* m_Instance;
+	};
+
+	template<typename T, typename ... Args>
+	constexpr Ref<T> CreateRef(Args&& ... args)
+	{
+		return Ref<T>::Create(std::forward<Args>(args)...);
+	}
+}
